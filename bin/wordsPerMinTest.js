555019409c06b0a5b32f143a89d37cb3460543ae
scoombe <skatesam95@gmail.com>
2018-07-18 12:32:55 +0100

    Initial commit

---
+++ bin/wordsPerMinTest.js
@@ -0,0 +1,165 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const randomWords = require("random-words");
+class wordsPerMinTest {
+    /**
+     * @param  {boolean} randomChars? if it is random chars or it is random words
+     */
+    constructor(randomChars) {
+        // holds the position that the user has got through the words
+        this.charPos = 0;
+        // the complete text from start to finish
+        this.CompleteText = "";
+        // the text that is being displayed 100 chars
+        this.curDisplayText = "";
+        // when the words per min test is finished used for making sure keys aren't checked when it is finished
+        this.done = false;
+        this.averageWPM = 0;
+        // the most recent words per minute 
+        this.lastTenAvWPM = 0;
+        this.secTimer = 0;
+        this.started = false;
+        this.wordCount = 0;
+        this.wordTimes = [];
+        this.highscore = { wpm: 0, averageWPM: 0, name: "" };
+        this.usingRandomChar = false;
+        if (randomChars) {
+            this.usingRandomChar = true;
+            this.generateChars(1000);
+        }
+        else {
+            this.generateText();
+        }
+    }
+    restartTest() {
+        this.charPos = 0;
+        this.CompleteText = "";
+        this.curDisplayText = "";
+        this.done = false;
+        this.averageWPM = 0;
+        this.lastTenAvWPM = 0;
+        this.secTimer = 0;
+        this.started = false;
+        this.wordCount = 0;
+        this.wordTimes = [];
+        if (this.usingRandomChar) {
+            this.generateChars(1000);
+        }
+        else {
+            this.generateText();
+        }
+    }
+    /**
+     * @param  {number} charLen length of the char string to create
+     * @returns void
+     */
+    generateChars(charLen) {
+        let charString = "";
+        let stringLength = 0;
+        while (stringLength != charLen) {
+            charString += this.randomChar();
+            if (stringLength % 5 == 0 && stringLength != 0 && stringLength + 1 != charLen) {
+                charString += " ";
+            }
+            stringLength = charString.length;
+        }
+        this.CompleteText = charString;
+        this.curDisplayText = this.CompleteText.slice(0, 100);
+    }
+    randomChar() {
+        const charList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", "!", "\"", "£", "$", "%", "^", "&", "*", ",", "(", ")", ":", "@", "{", "}", "#", "?", ">", "<", "[", "]", "/", "\\", "<", ">", "\'", ";"];
+        let randomNum = Math.floor((Math.random() * charList.length));
+        return charList[randomNum];
+    }
+    //method for generating random words 
+    generateText() {
+        let randWords = randomWords({ exactly: 200, maxLength: 5 });
+        this.CompleteText = randWords.join(" ");
+        this.curDisplayText = this.CompleteText.slice(0, 100);
+    }
+    // function for getting 1 character
+    getCurrentChar() {
+        return this.CompleteText.slice(this.charPos, this.charPos + 1);
+    }
+    /**
+     * @function:  getting the average time to do 5 chars and then calculating the average words per minute
+     * @returns void
+     */
+    calcAverageWPM() {
+        let totalTimes = 0;
+        let count = 0;
+        this.wordTimes.forEach(time => {
+            totalTimes += time;
+            count++;
+        });
+        // average time for a word to be written
+        let averageTime;
+        averageTime = totalTimes / count;
+        this.averageWPM = 60 / (averageTime / 1000);
+        //calling the mostRecentWPM func
+        this.mostRecentWPM();
+    }
+    /**
+     * @function: getting the words per minute from the last 10 words instead of all of the words
+     * @returns: void
+     */
+    mostRecentWPM() {
+        let totalTimes = 0;
+        let count = 0;
+        let greaterThan10 = this.wordTimes.length > 10;
+        let startingNum = greaterThan10 ? this.wordTimes.length - 10 : 0;
+        for (var i = startingNum; i < this.wordTimes.length; i++) {
+            totalTimes += this.wordTimes[i];
+        }
+        let averageTime;
+        averageTime = greaterThan10 ? totalTimes / 10 : totalTimes / this.wordTimes.length;
+        this.lastTenAvWPM = 60 / (averageTime / 1000);
+    }
+    /**
+     * @function for checking if the key char is correct
+     * @param  {string} keyPressChar
+     * @returns {json} ReturnObj: isCharCorrect - bool: newWord - bool: ?errorText - string
+     *
+     *
+     */
+    checkKeyChar(keyPressChar) {
+        let currentChar = this.getCurrentChar();
+        let returnObj = { newWord: false, isCharCorrect: false };
+        if (this.started) {
+            if (currentChar == keyPressChar) {
+                if (this.charPos != 0 && this.charPos % 5 == 0) {
+                    returnObj.newWord = true;
+                }
+                if (currentChar == " ") {
+                    this.wordCount++;
+                }
+                this.charPos = this.charPos + 1;
+                this.curDisplayText = this.CompleteText.slice(this.charPos, this.charPos + 100);
+                returnObj.isCharCorrect = true;
+            }
+            else {
+                if (currentChar == " ") {
+                    returnObj.errorText = "[space]";
+                }
+                else {
+                    returnObj.errorText = currentChar;
+                }
+            }
+        }
+        return returnObj;
+    }
+    checkHighscore() {
+        if (this.wordCount * 2 > this.highscore.wpm) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    updateHighscore(userName) {
+        const newScore = { name: userName, wpm: this.wordCount * 2, averageWPM: this.averageWPM };
+        this.highscore = newScore;
+    }
+}
+exports.wordsPerMinTest = wordsPerMinTest;
+//# sourceMappingURL=wordsPerMinTest.js.map
---
+++ test/typingTest.test.ts
@@ -0,0 +1,171 @@
+
+import {expect} from 'chai';
+import {wordsPerMinTest} from "../src/wordsPerMinTest";
+
+
+const wordsTest = new wordsPerMinTest;
+describe( "Average Words Per Minute Test", function() { 
+    it( "should have an correct words per mins", function() {
+        // an average of one word per second
+        wordsTest.wordTimes = [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000];
+        wordsTest.calcAverageWPM();
+        expect(wordsTest.averageWPM).to.equal(60);
+        // an average of 75 words per min
+        wordsTest.wordTimes = [800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800];
+        wordsTest.calcAverageWPM();
+        expect(wordsTest.averageWPM).to.equal(75);
+        // an average of 50 words per min
+        wordsTest.wordTimes = [1200,1200,1200,1200,1200,1200,1200,1200,1200,1200,1200];
+        wordsTest.calcAverageWPM();
+        expect(wordsTest.averageWPM).to.equal(50);
+    });
+    it ("Should average out correctly",function(){
+        // Should average out to one word a second
+        wordsTest.wordTimes = [1200, 1000, 800];
+        wordsTest.calcAverageWPM();
+        expect(wordsTest.averageWPM).to.equal(60);
+        // should average out as 1061.375 millie seconds to do a word
+        // divide by 1000 and divide 60 by the result.
+        wordsTest.wordTimes = [1200, 1000, 800, 999, 898, 1299, 1237, 1058];
+        wordsTest.calcAverageWPM();
+        expect(wordsTest.averageWPM).to.equal(56.53044399952891);
+    })
+});
+
+describe( "Check Key Char test", function() {
+    wordsTest.started = true;
+    wordsTest.CompleteText = "aaaaa aaaaa aaaaa "
+    it( "should check correct chars", function() {
+
+        let checkKeyCharObj  = wordsTest.checkKeyChar("a");
+        expect( checkKeyCharObj.isCharCorrect ).to.be.true;
+        expect( checkKeyCharObj.newWord).to.be.false;
+    })
+    it( "should return error text", function() {
+        let checkKeyCharObj = wordsTest.checkKeyChar("b");
+        expect( checkKeyCharObj.isCharCorrect ).to.be.false;
+        expect( checkKeyCharObj.newWord).to.be.false;
+        expect( checkKeyCharObj.errorText).to.equal("a");
+        wordsTest.checkKeyChar("a");
+        wordsTest.checkKeyChar("a");
+        wordsTest.checkKeyChar("a");
+        wordsTest.checkKeyChar("a");
+        checkKeyCharObj = wordsTest.checkKeyChar("a");
+        expect( checkKeyCharObj.isCharCorrect ).to.be.false;
+        expect( checkKeyCharObj.newWord).to.be.false;
+        expect( checkKeyCharObj.errorText).to.equal("[space]");
+    })
+    it( "should be a new word", function() {
+        let checkKeyCharObj = wordsTest.checkKeyChar(" ");
+        expect( checkKeyCharObj.isCharCorrect ).to.be.true;
+        expect( checkKeyCharObj.newWord).to.be.true;
+        expect(wordsTest.wordCount).to.equal(1);
+    })
+})
+
+describe ( "random Char tests", function() {
+    it ( "should only return chars", function() {
+        for (let i; i < 1000; i ++) {
+            expect(typeof(wordsTest.randomChar)).to.equal("string");
+        }
+    });
+})
+
+describe ( "generating random Chars", function() {
+    it ( "should return a certain number of random chars", function() {
+        wordsTest.generateChars(100);
+        expect(wordsTest.CompleteText.length).to.equal(100);
+    });
+    it ( " should have spaces in it ever 5th char from the 6th pos", function() {
+        for(let i = 6; i < 100; i += 5)
+        {
+            expect(wordsTest.CompleteText.slice(i,i+1)).to.equal(" ");
+        }       
+    });
+})
+
+describe ( "generating random words ", function() {
+    it ( "should generate 200 words in the complete text", function() {
+        wordsTest.generateText();
+        expect(wordsTest.CompleteText.split(" ").length).to.equal(200);
+    });
+    it ( "should have 100 chars in the display text", function() {
+        wordsTest.generateText();
+        expect(wordsTest.curDisplayText.length).to.equal(100);
+    });
+    it ( "should only have strings in the complete text", function() {
+        wordsTest.CompleteText.split(" ").forEach(word => {
+            expect(typeof(word)).to.equal("string");
+        });
+    });
+})
+
+describe ( "constructor tests", function() {
+    it ( "should have 1000 random chars in the complete text", function() {
+        let newWordTest = new wordsPerMinTest(true);
+        expect(newWordTest.CompleteText.length).to.equal(1000);
+    });
+    it ( "should have 200 words in the complete text" , function() {
+        let newWordTest = new wordsPerMinTest(false);
+        expect(newWordTest.CompleteText.split(" ").length).to.equal(200)
+    });
+    it ( "should have 200 words in the complete text without specifying the random chars bool" , function() {
+        let newWordTest = new wordsPerMinTest;
+        expect(newWordTest.CompleteText.split(" ").length).to.equal(200)
+    });
+})
+
+describe ( " most recent wpm tests", function () {
+    it( "should have an correct words per mins", function() {
+        // an average of one word per second
+        wordsTest.wordTimes = [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000];
+        wordsTest.mostRecentWPM();
+        expect(wordsTest.lastTenAvWPM).to.equal(60);
+        // an average of 75 words per min
+        wordsTest.wordTimes = [800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800,800];
+        wordsTest.mostRecentWPM();
+        expect(wordsTest.lastTenAvWPM).to.equal(75);
+        // an average of 50 words per min
+        wordsTest.wordTimes = [1200,1200,1200,1200,1200,1200,1200,1200,1200,1200,1200];
+        wordsTest.mostRecentWPM();
+        expect(wordsTest.lastTenAvWPM).to.equal(50);
+    });
+    it ("Should average out correctly",function(){
+        // Should average out to one word a second
+        wordsTest.wordTimes = [1200, 1000, 800];
+        wordsTest.mostRecentWPM();
+        expect(wordsTest.lastTenAvWPM).to.equal(60);
+        // should average out as 1061.375 millie seconds to do a word
+        // divide by 1000 and divide 60 by the result.
+        wordsTest.wordTimes = [1200, 1000, 800, 999, 898, 1299, 1237, 1058];
+        wordsTest.mostRecentWPM();
+        expect(wordsTest.lastTenAvWPM).to.equal(56.53044399952891);
+    })
+})
+
+describe ( " highscores are updated ", function () {
+    it ( "should check if it's a high score" , function() {
+        wordsTest.wordCount = 30;
+        expect(wordsTest.checkHighscore()).to.be.true;
+        wordsTest.updateHighscore("sam")
+        
+        wordsTest.wordCount = 60;
+        expect(wordsTest.checkHighscore()).to.be.true;
+        wordsTest.updateHighscore("sam");
+        
+        wordsTest.wordCount = 120;
+        expect(wordsTest.checkHighscore()).to.be.true;  
+        wordsTest.updateHighscore("sam");
+
+        wordsTest.wordCount = 60;
+        expect(wordsTest.checkHighscore()).to.be.false;
+    })
+    it ( " update a highscore " , function() {
+        let newWordTest = new wordsPerMinTest;
+        newWordTest.averageWPM = 120;
+        newWordTest.wordCount = 60;
+        newWordTest.updateHighscore("sam");
+        expect(newWordTest.highscore).to.deep.equal({wpm: 120, averageWPM: 120, name: "sam"});
+    })
+
+})
